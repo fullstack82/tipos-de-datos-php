dd56515727c912d1b354ced9a0831a80b2ac998e
fullstack82 <fullstack8210@gmail.com>
2021-01-28 08:51:01 +0100

    tipos de datos en php

--- index.php
+++ index.php
@@ -4,6 +4,32 @@
 /*
     TIPOS DE DATOS:
     Entero (int / intenger)   99
-
-
+    Coma flotante / decimales (float / double) = 3,45
+    Cadenas (string) = "Hola yo soy un string"
+    Boleano (bool) = 1  o 0 , true o false
+    Null 
+    Array (coleccion de objetos)
+    Objetos
 */
+
+// Usando el tipo de datos;
+$numero = 100;  // integer
+$decimal = 27.9; //double
+$texto = "soy un texto";  // string
+$verdadero = true; // boolean
+
+
+
+//Obtener dato visualmente
+echo gettype($numero); // integer
+echo "<br>" ;
+echo gettype($decimal); // double
+echo "<br>" ;
+echo gettype($texto); // string
+echo "<br>" ;
+echo gettype($verdadero); // boolean
+echo "<br>" ;
+
+
+
+
